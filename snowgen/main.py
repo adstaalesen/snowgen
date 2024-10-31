171383064bbc70ce96cd269c738a47c8c9c016bf
Adrian Stalesen <adrian.stalesen@klp.no>
2024-10-31 17:51:33 +0100

    Non tupling the object path

--- snowgen/main.py
+++ snowgen/main.py
@@ -21,16 +21,16 @@ def save_objects(
         )
 
         print(obj)
+
         ddl = snowflake_object.get_ddl(
             sql_template=database_repository.get_sql_template(
                 template_name=obj["template_name"]
             )
         )
-        object_path = (
-            snowflake_object.generate_object_path(
-                database_repository.snowflake_objects_path
-            ).resolve(),
-        )
+
+        object_path = snowflake_object.generate_object_path(
+            database_repository.snowflake_objects_path
+        ).resolve()
 
         database_repository.save_database_object(ddl, object_path, replace=replace)
 
